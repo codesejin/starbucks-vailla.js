71718163a960f5f6cd0407428d8f85184c9f93a4
sejin park <seijin0722@naver.com>
2023-09-07 01:50:07 +0900

    뱃지 GSAP

--- js/main.js
+++ js/main.js
@@ -15,3 +15,25 @@ searchInputEl.addEventListener('blur', function() {
   searchEl.classList.remove('focused');
   searchInputEl.setAttribute('placeholder', '');
 });
+
+const badgeEl = document.querySelector('header .badges');
+
+// 화면자체에 스크롤 이벤트를 추가해서, 스크롤되면 함수를 실행하겠다는 의미
+window.addEventListener('scroll', _.throttle(function() {
+  console.log(window.scrollY)
+  if(window.scrollY > 500){
+    // 배지 숨기기
+    // gsap.to(요소, 지속시간, 어떻게 처리할지 옵션);
+    gsap.to(badgeEl, .6, {
+      opacity: 0,
+      display: 'none'
+    });
+  } else {
+    // 배지 보여주기
+    gsap.to(badgeEl, .6, {
+      opacity: 1,
+      display: 'block'
+    });
+  }
+}, 300));
+// _.throttle(함수, 시간ms)
