83bfaf3dfd6f176ec56d7b906381a8e57788ed4b
sejin park <seijin0722@naver.com>
2023-09-07 23:52:00 +0900

    gsap 순차적인 애니메이션

--- js/main.js
+++ js/main.js
@@ -37,3 +37,13 @@ window.addEventListener('scroll', _.throttle(function() {
   }
 }, 300));
 // _.throttle(함수, 시간ms)
+
+
+const fadeEls = document.querySelectorAll('.visual .fade-in');
+fadeEls.forEach(function (fadeEl, index) {
+  // gsap.to(요소, 지속시간, 어떻게 처리할지 옵션);
+  gsap.to(fadeEl, 1, {
+    delay: (index + 1) * .7, // 0.7, 1.4, 2.1, 2.7
+    opacity: 1
+  });
+});
